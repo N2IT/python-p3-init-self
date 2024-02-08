f08e665f58bac931e4f2b854191f5b4b3662816a
Tony Eder <tonyeder11@gmail.com>
2024-02-08 10:39:22 -0800

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+
+    # def bark(self):
+    #     print("Woof!")
+
+    # def showing_self(self):
+    #     return self
+
+    # def get_adopted(self, owner_name):
+    #     self.owner = owner_name
+
+
+
+
+# fido = Dog("Fido")
+# fido.get_adopted("Sophie")
+# fido.owner
+
+# snoopy = Dog("Snoopy", "Tennis Ball")
+# snoopy.favorite_toy
+
+
